873340f94ca21764ae5fb3efdb2b350d6b223360
rezmotaqi <rezmotaqi@gmail.com>
2022-08-28 16:27:12 +0430

    add products

---
+++ app/schemas/products.py
@@ -0,0 +1,114 @@
+from typing import Optional, List
+
+from pydantic.fields import Field
+
+from app.schemas.base import Model, ObjectId, DateTime
+
+
+class ProductGuaranteeInput(Model):
+    """
+    Pydantic schema for product guarantee
+    """
+
+    name: str = Field()
+    price: int = Field()
+    expire_duration: int = Field(description="amount of time in days")
+    image: Optional[str] = Field(description="path of uploaded file")
+
+
+class ProductGuaranteeOutput(Model):
+    """
+    Pydantic schema for returning guarantees
+    """
+
+    name: str = Field()
+    price: int = Field()
+    expire_duration: int = Field(description="amount of time in days")
+    image: Optional[str] = Field(description="path of uploaded file")
+
+
+class ProductGuaranteeListOutput(Model):
+    """
+    Pydantic schema for list of guarantees
+    """
+
+    guarantees: List[ProductGuaranteeOutput] = Field()
+    count: int = Field()
+
+
+class ProductGuaranteeInProduct(Model):
+    """
+    Pydantic schema for product guarantee data in product
+    """
+
+    id: ObjectId = Field()
+    name: str = Field()
+    price: int = Field()
+    expire_duration: int = Field(description="amount of time in days")
+    image: Optional[str] = Field(description="path of uploaded file")
+
+
+class ProductTrait(Model):
+    """
+    Pydantic schema for product traits in product
+    """
+
+    name: str = Field()
+    value: str = Field()
+
+
+class ProductInput(Model):
+    """
+    Pydantic schema for creating product
+    """
+
+    farsi_name: Optional[str] = Field()
+    english_name: str = Field()
+    slug: Optional[str] = Field()
+    tab_title: Optional[str] = Field()
+    keywords: List[str] = Field()
+    publish_date: DateTime = Field()
+    price: int = Field()
+    price_update_date: DateTime = Field()
+    is_active: bool = Field()
+    is_available: bool = Field()
+    traits: Optional[List[ProductTrait]] = Field()
+    guarantees: Optional[List[ProductGuaranteeInProduct]] = Field()
+    stock: int = Field(gt=0)
+    discount_percent: Optional[int] = Field(description="product internal discount")
+    images: Optional[List[str]] = Field()
+
+    # model: str = Field()
+    # tags: stock = Field()
+    # category: str = Field()
+
+
+class ProductOutput(Model):
+    """
+    Pydantic schema for returning product
+    """
+
+    farsi_name: Optional[str] = Field()
+    english_name: str = Field()
+    slug: Optional[str] = Field()
+    tab_title: Optional[str] = Field()
+    keywords: List[str] = Field()
+    publish_date: DateTime = Field()
+    price: int = Field()
+    price_update_date: DateTime = Field()
+    is_active: bool = Field()
+    is_available: bool = Field()
+    traits: Optional[List[ProductTrait]] = Field()
+    guarantees: Optional[List[ProductGuaranteeInProduct]] = Field()
+    stock: int = Field(gt=0)
+    discount_percent: Optional[int] = Field(description="product internal discount")
+    images: Optional[List[str]] = Field()
+
+
+class ProductListOutput(Model):
+    """
+    Pydantic schema for list of guarantees
+    """
+
+    products: List[ProductOutput] = Field()
+    count: int = Field()
